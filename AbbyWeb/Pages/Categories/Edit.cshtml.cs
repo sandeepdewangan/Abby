84957a62bec99c9f5945aabcbce61fe46be199d1
sandeepdewangan <sandeepdewangan7@gmail.com>
2022-07-17 09:49:27 +0530

    Category - Update

---
+++ AbbyWeb/Pages/Categories/Edit.cshtml.cs
@@ -0,0 +1,43 @@
+using AbbyWeb.Data;
+using AbbyWeb.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AbbyWeb.Pages.Categories
+{
+    public class EditModel : PageModel
+    {
+        [BindProperty]
+        public Category Category { get; set; }
+        public ApplicationDbContext _db { get; set; }
+
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void OnGet(int Id)
+        {
+            Category = _db.Category.Find(Id);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (Category.Name == Category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Category.Name", "The display order cannot exactly match the Name.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Category.Update(Category);
+                await _db.SaveChangesAsync();
+
+                return RedirectToPage("Index");
+            }
+
+            return Page();
+
+        }
+    }
+}
